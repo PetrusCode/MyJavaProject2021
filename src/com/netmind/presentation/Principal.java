3fbf4100889f57e98fd7d57ba729e7baf696b8d9
petrusCode <pedro_hot32@hotmail.com>
2021-02-17 17:33:33 +0100

    initial commit, with calculator structure

---
+++ src/com/netmind/presentation/Principal.java
@@ -0,0 +1,12 @@
+package com.netmind.presentation;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Los text de integracion son los que prueban el codigo real y los servicios reales
+		
+		System.out.println("This is my first java class");
+	}
+
+}

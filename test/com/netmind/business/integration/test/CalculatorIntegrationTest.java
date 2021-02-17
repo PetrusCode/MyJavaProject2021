434c9622d14b62cac8c12e34fcedf25090096767
petrusCode <pedro_hot32@hotmail.com>
2021-02-17 19:34:48 +0100

    Calculator integration test implemented

---
+++ test/com/netmind/business/integration/test/CalculatorIntegrationTest.java
@@ -0,0 +1,36 @@
+package com.netmind.business.integration.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.netmind.business.Calculator;
+
+public class CalculatorIntegrationTest {
+	//Tests de integracion
+	@Test
+	public void testAdd() {
+		Calculator calculator = new Calculator();
+		assertEquals(calculator.add(4,2), 6);
+		
+	}
+
+	@Test
+	public void testSubstract() {
+		Calculator calculator2 = new Calculator();
+		assertEquals(calculator2.substract(5, 5), 0);
+	}
+
+	@Test
+	public void testMultiply() {
+		Calculator calculator3 = new Calculator();
+		assertEquals(calculator3.multiply(5, 5), 25);
+	}
+
+	@Test
+	public void testDivide() {
+		Calculator calculator4 = new Calculator();
+		assertEquals(calculator4.divide(100, 100),1);
+	}
+
+}
